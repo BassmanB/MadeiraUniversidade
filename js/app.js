a3268f89e7e85ca55cffe81a3725c5d4c37ebf04
PolakoPreto <daniel.czarnozynski@gmail.com>
2018-06-21 14:18:37 +0200

    second commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation();
+
+$('.title-bar').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('shrink');
+})
+
+
+function initMap() {
+        var uluru = {lat: 32, lng: -19};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
